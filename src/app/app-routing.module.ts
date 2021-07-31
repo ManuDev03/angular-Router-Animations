826f6df8def9260d61c7b1a70d255c735fd8d43b
ManuDev03 <manuprajan03@gmail.com>
2021-07-31 19:31:10 +0530

    fireship-Angular Router-animations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { LeftPageComponent } from './left-page/left-page.component';
+import { RightPageComponent } from './right-page/right-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'about', component:AboutPageComponent},
+  {path:'left', component:LeftPageComponent, data:{animation: 'isRight'} },
+  {path:'right', component:RightPageComponent,data:{animation: 'isLeft'}}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RightPageComponent } from './right-page/right-page.component';
+import { LeftPageComponent } from './left-page/left-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RightPageComponent,
+    LeftPageComponent,
+    AboutPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
